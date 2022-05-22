a6cdd9ae639de10ba0c3d2ad581229cd49561ad3
이현민 <2017030182@hanyang.ac.kr>
2022-05-22 22:24:45 +0900

    Tidy up thread.c

--- xv6-public/thread.c
+++ xv6-public/thread.c
@@ -53,11 +53,56 @@ threads_apply1(struct proc* p,
   return 0;
 }
 
-struct proc*
+////////////
+
+static struct proc*
 __routine_get_thread(struct proc* th, void* tid)
 {
   return th->tid == (thread_t)tid ? th : 0;
 }
+
+static struct proc*
+__routine_handle_orphan_thread(struct proc* th, void* main)
+{
+  struct proc *thmain = (struct proc*)main;
+  if(th->thmain == thmain)
+    th->thmain = main_thread(thmain);
+  return 0;
+}
+
+static struct proc*
+__routine_is_ready(struct proc* th)
+{
+  return th->state == RUNNABLE ? th : 0;
+}
+
+static struct proc*
+__routine_is_sleeping(struct proc* th)
+{
+  return th->state == SLEEPING ? th : 0;
+}
+
+static struct proc*
+__routine_is_ready_or_running(struct proc* th)
+{
+  return th->state == RUNNABLE || th->state == RUNNING ? th : 0;
+}
+
+static struct proc*
+__routine_kill_thread(struct proc* th)
+{
+  th->killed = 1;
+  return 0;
+}
+
+static struct proc*
+__routine_usurp_proc(struct proc *th, void *main)
+{
+  th->thmain = (struct proc*)main;
+  return 0;
+}
+
+////////////
 
 static struct proc*
 __get_thread(thread_t thread)
@@ -66,6 +111,49 @@ __get_thread(thread_t thread)
                         __routine_get_thread,
                         (void*)thread);
 }
+
+static void
+__free_thread(struct proc *th)
+{
+  kfree(th->kstack);
+  th->kstack = 0;
+  deallocustack(th->pgdir, th->ustack);
+  th->pid = 0;
+  th->tid = 0;
+  th->parent = 0;
+  th->name[0] = 0;
+  th->killed = 0;
+  th->tickets = 0;
+  th->pass = 0;
+  th->ticks = 0;
+  th->privlevel = 0;
+  th->retval = 0;
+  th->state = UNUSED;
+  list_add(&th->free, &ptable.free);
+}
+
+static void
+__usurp_proc(struct proc *th)
+{
+  int i;
+  struct proc *thmain = main_thread(th);
+
+  th->sz = thmain->sz;
+  th->ticks = thmain->ticks;
+  for(i = 0; i < NOFILE; i++)
+    if(thmain->ofile[i])
+      th->ofile[i] = thmain->ofile[i];
+  th->cwd = thmain->cwd;
+  if(th->type == STRIDE){
+    th->tickets = thmain->tickets;
+    th->pass = thmain->pass;
+  }
+  threads_apply1(th, __routine_usurp_proc, th);
+  thmain->tid = th->tid;
+  th->tid = 0;
+}
+
+//////////
 
 struct proc*
 main_thread(struct proc *th)
@@ -76,21 +164,9 @@ main_thread(struct proc *th)
 }
 
 struct proc*
-__routine_is_ready(struct proc* th)
-{
-  return th->state == RUNNABLE ? th : 0;
-}
-
-struct proc*
 ready_thread(struct proc *th)
 {
   return threads_apply0(th, __routine_is_ready);
-}
-
-struct proc*
-__routine_is_sleeping(struct proc* th)
-{
-  return th->state == SLEEPING ? th : 0;
 }
 
 struct proc*
@@ -100,17 +176,41 @@ sleeping_thread(struct proc *th)
 }
 
 struct proc*
-__routine_is_ready_or_running(struct proc* th)
-{
-  return th->state == RUNNABLE || th->state == RUNNING ? th : 0;
-}
-
-struct proc*
 ready_or_running_thread(struct proc *th)
 {
   return threads_apply0(th, __routine_is_ready_or_running);
 }
 
+//////////
+void
+terminate_proc(struct proc* p){
+  threads_apply0(p, __routine_kill_thread);
+}
+
+int
+monopolize_proc(struct proc *p)
+{
+  struct proc *th;
+  int null = 0;
+
+  acquire(&ptable.lock);
+  wakeup1(p->thmain);
+  if(p != p->thmain)
+    __usurp_proc(p);
+  terminate_proc(p);
+  p->killed = 0;
+  release(&ptable.lock);
+
+  while(!list_empty(&p->thgroup)){
+    th = list_first_entry(&p->thgroup,
+                          struct proc, thgroup);
+    if(thread_join(th->tid, (void**)&null) < 0)
+      return -1;
+  }
+  return 0;
+}
+
+/////
 int
 thread_create(thread_t *thread,
               void *(*start_routine)(void *),
@@ -187,15 +287,6 @@ thread_create(thread_t *thread,
   return 0;
 }
 
-struct proc*
-__handle_orphan_thread(struct proc* th, void* main)
-{
-  struct proc *thmain = (struct proc*)main;
-  if(th->thmain == thmain)
-    th->thmain = main_thread(thmain);
-  return 0;
-}
-
 void
 thread_exit(void *retval)
 {
@@ -215,33 +306,13 @@ thread_exit(void *retval)
   curth->retval = retval;
   wakeup1(curth->thmain);
 
-  threads_apply1(curth, __handle_orphan_thread, curth);
+  threads_apply1(curth, __routine_handle_orphan_thread, curth);
 
   if(curth->type == MLFQ)
     dequeue_thread(curth);
   curth->state = ZOMBIE;
   sched();
   panic("zombie thread exit");
-}
-
-static void
-freethread(struct proc *th)
-{
-  kfree(th->kstack);
-  th->kstack = 0;
-  deallocustack(th->pgdir, th->ustack);
-  th->pid = 0;
-  th->tid = 0;
-  th->parent = 0;
-  th->name[0] = 0;
-  th->killed = 0;
-  th->tickets = 0;
-  th->pass = 0;
-  th->ticks = 0;
-  th->privlevel = 0;
-  th->retval = 0;
-  th->state = UNUSED;
-  list_add(&th->free, &ptable.free);
 }
 
 int
@@ -261,7 +332,7 @@ thread_join(thread_t thread, void **retval)
       *retval = th->retval;
       list_del(&th->thgroup);
       list_del(&th->sibling);
-      freethread(th);
+      __free_thread(th);
       release(&ptable.lock);
       return 0;
     }
@@ -269,68 +340,4 @@ thread_join(thread_t thread, void **retval)
   }
 }
 
-struct proc*
-__routine_kill_thread(struct proc* th)
-{
-  th->killed = 1;
-  return 0;
-}
 
-void
-terminate_proc(struct proc* p){
-  threads_apply0(p, __routine_kill_thread);
-}
-
-struct proc*
-__routine_usurp_proc(struct proc *th, void *main)
-{
-  th->thmain = (struct proc*)main;
-  return 0;
-}
-
-// usurp_main doesn't guarantee the order of threads.
-// If it is called, the other threads have to be
-// terminated.
-static void
-__usurp_proc(struct proc *th)
-{
-  int i;
-  struct proc *thmain = main_thread(th);
-
-  th->sz = thmain->sz;
-  th->ticks = thmain->ticks;
-  for(i = 0; i < NOFILE; i++)
-    if(thmain->ofile[i])
-      th->ofile[i] = thmain->ofile[i];
-  th->cwd = thmain->cwd;
-  if(th->type == STRIDE){
-    th->tickets = thmain->tickets;
-    th->pass = thmain->pass;
-  }
-  threads_apply1(th, __routine_usurp_proc, th);
-  thmain->tid = th->tid;
-  th->tid = 0;
-}
-
-int
-monopolize_proc(struct proc *p)
-{
-  struct proc *th;
-  int null = 0;
-
-  acquire(&ptable.lock);
-  wakeup1(p->thmain);
-  if(p != p->thmain)
-    __usurp_proc(p);
-  terminate_proc(p);
-  p->killed = 0;
-  release(&ptable.lock);
-
-  while(!list_empty(&p->thgroup)){
-    th = list_first_entry(&p->thgroup,
-                          struct proc, thgroup);
-    if(thread_join(th->tid, (void**)&null) < 0)
-      return -1;
-  }
-  return 0;
-}
