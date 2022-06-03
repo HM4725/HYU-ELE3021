df6cbff4a450c6bb5d0569f52a00d294cc7c39af
이현민 <2017030182@hanyang.ac.kr>
2022-06-04 06:14:18 +0900

    Increase filesystem size

--- xv6-public/param.h
+++ xv6-public/param.h
@@ -11,7 +11,7 @@
 #define MAXOPBLOCKS    10    // max # of blocks any FS op writes
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
-#define FSSIZE       1000    // size of file system in blocks
+#define FSSIZE       2000    // size of file system in blocks
 #define QSIZE           3    // total levels size of mlfq
 #define BOOSTINTERVAL 200    // ticks interval of priority boost
 #define RESERVE        20    // required tickets reserve of mlfq 
