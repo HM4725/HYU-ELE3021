5ac00fd180351e0ae0d1d50f5cbae4060f542a99
이현민 <2017030182@hanyang.ac.kr>
2022-06-04 17:09:18 +0900

    Increase NPROC for thread test

--- xv6-public/param.h
+++ xv6-public/param.h
@@ -1,4 +1,4 @@
-#define NPROC          64    // maximum number of processes
+#define NPROC         256    // maximum number of processes
 #define KSTACKSIZE   4096    // size of per-process kernel stack
 #define USTACKSIZE   4096    // size of per-process user stack
 #define NCPU            8    // maximum number of CPUs
